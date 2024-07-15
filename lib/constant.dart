8e99b86b5c4ca03d8a685c188d0fe6bcde67d50e
mac <>
2024-07-15 12:38:16 +0530

    Android version update to 34

--- lib/constant.dart
+++ lib/constant.dart
@@ -6,7 +6,7 @@ const venue = "48 Marion St, Harris Park NSW 2150, Australia";
 const call = "02-9635 1777";
 const appColor = Colors.deepOrange;
 
-const orderURL = "https://jaipursweets.com.au/orders-online/";
+const orderURL = "https://jaipursweets.websiteorders.com.au/orders-online/";
 const galleryURL = "https://jaipursweets.com.au/gallery";
 const menuURL = "https://jaipursweets.com.au/menu";
 const facebookURL = "https://www.facebook.com/people/Jaipur-Sweets-Harris-Park/100063791250107/";
